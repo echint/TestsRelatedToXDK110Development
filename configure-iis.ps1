3a7f26976e6e9a6d76c2cb137da0bbc430e33c63
echint <47112286+echint@users.noreply.github.com>
2019-01-28 15:38:40 +0100

    Add files via upload

---
+++ configure-iis.ps1
@@ -0,0 +1,4 @@
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Welcome to Azure! My name is Ervan Chint, and Onur is a Tombalak.</h2></body></html>"
